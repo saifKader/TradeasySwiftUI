ea2e80b8a90af3c2d4c37db23d84d96fd8fd594c
Seif eddine <61545317+saifKader@users.noreply.github.com>
2023-03-14 19:33:22 +0100

    clean

--- Pods/Target Support Files/Pods-TradeasySwiftUI-TradeasySwiftUIUITests/Pods-TradeasySwiftUI-TradeasySwiftUIUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TradeasySwiftUI_TradeasySwiftUIUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TradeasySwiftUI_TradeasySwiftUIUITests
-@end
--- Pods/Target Support Files/Pods-TradeasySwiftUITests/Pods-TradeasySwiftUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TradeasySwiftUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TradeasySwiftUITests
-@end
